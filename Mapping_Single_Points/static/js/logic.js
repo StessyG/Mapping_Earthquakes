42c6a629f0bc13316abab8fbefcb5b99f783aafd
Stessy Iriho <stessyiriho@gmail.com>
2022-01-30 14:37:03 -0600

    Single Points

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -7,7 +7,7 @@ let map = L.map('mapid').setView([40.7, -94.5], 14);
 L.circle([34.0522, -118.2437], {
     radius: 300,
     color: "black",
-    fillColor: '#ffffe0'
+    fillColor: '#ffffa1'
  }).addTo(map);
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
